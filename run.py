2626365af1061121b4dc17d5419c200a81cadf2e
test <test>
2022-12-03 14:29:04 +0800

    1

--- run.py
+++ run.py
@@ -191,9 +191,9 @@ if __name__ == '__main__':
     for md5 in [md5 for md5 in data.keys() if md5 not in md5s]:
         del data[md5]
     # 写入README.md
-    readme_md = '## pocsuite3 ({}个) 更新于 {}\n'.format(
+    readme_md = '## pocsuite3 (共{}个) 最近一次检查时间 {}\n'.format(
         len(data.keys()), time.strftime("%Y-%m-%d %H:%M:%S"))
-    readme_md += '### 更新记录\n| 文件名称 | 收录时间 |\n| :----| :---- |\n'
+    readme_md += '### 收集记录\n| 文件名称 | 收录时间 |\n| :----| :---- |\n'
     _data = sorted(data.values(), key=lambda x: x['up_time'], reverse=True)
     for item in _data:
         readme_md += '| [{}]({}) | {} |\n'.format(item['name'],
